202d203c1d8e19ac41dbf0685d1e756dd484fb7b
tothm23 <tothm.113sz@acsjszki.hu>
2023-04-17 14:48:06 +0200

    Generate card component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,31 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'mcdonalds-clone';
+
+  posts = [
+    {
+      title: 'BBQ Bob®',
+      imageUrl: 'assets/bbqbob.jfif',
+      kcal: 802,
+    },
+    {
+      title: 'Big Mac®',
+      imageUrl: 'assets/bigmac.jfif',
+      kcal: 513,
+    },
+    {
+      title: 'Hamburger',
+      imageUrl: 'assets/hamburger.jfif',
+      kcal: 250,
+    },
+    {
+      title: 'Maestro Honey Holly',
+      imageUrl: 'assets/maestro_honeyholly',
+      kcal: 665,
+    },
+  ];
 }
